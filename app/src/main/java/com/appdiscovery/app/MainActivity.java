20fd15ff57b193a1d3fa7946045a904f9b98f147
Ke Wang <453587854@qq.com>
2018-04-27 19:42:08 +0800

    重构代码

--- app/src/main/java/com/appdiscovery/app/MainActivity.java
+++ app/src/main/java/com/appdiscovery/app/MainActivity.java
@@ -1,15 +1,21 @@
 package com.appdiscovery.app;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 
-import com.appdiscovery.app.services.DiscoverApp;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -23,6 +29,28 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private WebApp[] webapps;
+
+    private static final int REQUEST_PERMISSION_LOCATION_STATE = 123;
+
+    private void showExplanation(String title,
+                                 String message,
+                                 final String permission,
+                                 final int permissionRequestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        requestPermission(permission, permissionRequestCode);
+                    }
+                });
+        builder.create().show();
+    }
+
+    private void requestPermission(String permissionName, int permissionRequestCode) {
+        ActivityCompat.requestPermissions(this,
+                new String[]{permissionName}, permissionRequestCode);
+    }
 
     MainActivity() {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -57,14 +85,55 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.apps_list);
         mRecyclerView.setHasFixedSize(true);
 
-        DiscoverApp.byGpsLocation(this, webapps -> {
-            this.webapps = webapps;
-            mAdapter = new AppListAdapter(webapps, onListItemClick);
-            mRecyclerView.setAdapter(mAdapter);
-            mLayoutManager = new LinearLayoutManager(this);
-            mRecyclerView.setLayoutManager(mLayoutManager);
-            return "";
-        });
-    }
 
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(Config.getInstance().repoServerAddr + "/app/discover?lat=" + location.getLatitude() + "&lng=" + location.getLongitude())
+                        .get()
+                        .build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    String jsonData = response.body().string();
+                    Gson gson = new Gson();
+                    MainActivity.this.webapps = gson.fromJson(jsonData, WebApp[].class);
+                    mAdapter = new AppListAdapter(webapps, onListItemClick);
+                    mRecyclerView.setAdapter(mAdapter);
+                    mLayoutManager = new LinearLayoutManager(MainActivity.this);
+                    mRecyclerView.setLayoutManager(mLayoutManager);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                showExplanation("Permission Needed", "Rationale", Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSION_LOCATION_STATE);
+            } else {
+                requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSION_LOCATION_STATE);
+            }
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+            return;
+        }
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        assert locationManager != null;
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+    }
 }
