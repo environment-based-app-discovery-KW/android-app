1712831cac181f6f7d6f58357e57ff459be583b1
Ke Wang <453587854@qq.com>
2018-04-27 19:48:13 +0800

    refactor code

--- app/src/main/java/com/appdiscovery/app/MainActivity.java
+++ app/src/main/java/com/appdiscovery/app/MainActivity.java
@@ -16,6 +16,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.appdiscovery.app.services.LoadApp;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -60,22 +61,7 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener onListItemClick = (view -> {
         int itemPosition = mRecyclerView.getChildLayoutPosition(view);
         WebApp webapp = webapps[itemPosition];
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(Config.getInstance().repoServerAddr + "/app/download?id=" + webapp.id)
-                .get()
-                .build();
-
-        try {
-            Response response = client.newCall(request).execute();
-            String jsonData = response.body().string();
-            Gson gson = new Gson();
-            WebApp app = gson.fromJson(jsonData, WebApp.class);
-            app.setContext(MainActivity.this);
-            app.downloadAndRun();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        LoadApp.show(this, webapp.id);
     });
 
     @Override
