6543100cf56150e68dabca2ca6496bc60026c5a0
Ke Wang <453587854@qq.com>
2018-04-27 19:15:50 +0800

    点击列表中的项目启动app

--- app/src/main/java/com/appdiscovery/app/MainActivity.java
+++ app/src/main/java/com/appdiscovery/app/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private WebApp[] webapps;
 
     MainActivity() {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -30,41 +31,34 @@ public class MainActivity extends AppCompatActivity {
 
     private View.OnClickListener onListItemClick = (view -> {
         int itemPosition = mRecyclerView.getChildLayoutPosition(view);
-        Log.d("itemClicked", String.valueOf(itemPosition));
+        WebApp webapp = webapps[itemPosition];
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(Config.getInstance().repoServerAddr + "/app/download?id=" + webapp.id)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            String jsonData = response.body().string();
+            Gson gson = new Gson();
+            WebApp app = gson.fromJson(jsonData, WebApp.class);
+            app.setContext(MainActivity.this);
+            app.downloadAndRun();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(view -> {
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(Config.getInstance().repoServerAddr + "/app/download?id=1")
-                    .get()
-                    .build();
-
-            try {
-                Response response = client.newCall(request).execute();
-                String jsonData = response.body().string();
-                Gson gson = new Gson();
-                WebApp app = gson.fromJson(jsonData, WebApp.class);
-                app.setContext(MainActivity.this);
-//                    Toast.makeText(MainActivity.this, app.name, Toast.LENGTH_SHORT).show();
-                app.downloadAndRun();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-//                Intent myIntent = new Intent(MainActivity.this, WebViewActivity.class);
-//                startActivity(myIntent);
-        });
         mRecyclerView = (RecyclerView) findViewById(R.id.apps_list);
         mRecyclerView.setHasFixedSize(true);
 
         DiscoverApp.byGpsLocation(this, webapps -> {
-            // specify an adapter (see also next example)
+            this.webapps = webapps;
             mAdapter = new AppListAdapter(webapps, onListItemClick);
             mRecyclerView.setAdapter(mAdapter);
             mLayoutManager = new LinearLayoutManager(this);
