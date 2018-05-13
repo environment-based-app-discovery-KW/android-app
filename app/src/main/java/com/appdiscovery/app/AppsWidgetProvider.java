414e6bc5b6cc2830abfa48e41000c8c36bae8bed
Ke Wang <453587854@qq.com>
2018-05-13 10:39:48 +0800

    fix widget not updating when in background

--- app/src/main/java/com/appdiscovery/app/AppsWidgetProvider.java
+++ app/src/main/java/com/appdiscovery/app/AppsWidgetProvider.java
@@ -1,12 +1,29 @@
 package com.appdiscovery.app;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.util.Random;
 
 public class AppsWidgetProvider extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Log.d("UpdateWidgetsService", "onReceive");
+        RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.app_widget_layout);
+        Log.d("UpdateWidgetsService", "updating..");
+        updateViews.setTextViewText(R.id.update, String.valueOf(new Random().nextInt(100)));
+        Intent pintent = new Intent(context, AppsWidgetProvider.class);
+        pintent.setAction("use_custom_class");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, pintent, PendingIntent.FLAG_UPDATE_CURRENT);
+        updateViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+        ComponentName thisWidget = new ComponentName(context, AppsWidgetProvider.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        manager.updateAppWidget(thisWidget, updateViews);
     }
 }
--- app/src/main/java/com/appdiscovery/app/services/WidgetAlarmService.java
+++ app/src/main/java/com/appdiscovery/app/services/WidgetAlarmService.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
 
+import com.appdiscovery.app.AppsWidgetProvider;
+
 public class WidgetAlarmService {
     public static boolean isStarted = false;
 
@@ -15,8 +17,9 @@ public class WidgetAlarmService {
         }
         isStarted = true;
 
-        final Intent intent = new Intent(context, UpdateWidgetsService.class);
-        final PendingIntent pending = PendingIntent.getService(context, 0, intent, 0);
+        final Intent intent = new Intent(context, AppsWidgetProvider.class);
+        intent.setAction("use_custom_class");
+        final PendingIntent pending = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         final AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         long interval = 60000;
         alarm.setRepeating(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime(), interval, pending);
