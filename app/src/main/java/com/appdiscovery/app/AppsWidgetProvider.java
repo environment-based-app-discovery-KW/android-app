82dd00972492e9f5bcebb7efd91eea8dea6e1ae3
Ke Wang <453587854@qq.com>
2018-05-13 14:05:21 +0800

    桌面小工具 - 根据location获得

--- app/src/main/java/com/appdiscovery/app/AppsWidgetProvider.java
+++ app/src/main/java/com/appdiscovery/app/AppsWidgetProvider.java
@@ -8,41 +8,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.net.Uri;
-import android.support.v7.widget.LinearLayoutManager;
-import android.util.Log;
-import android.view.View;
-import android.widget.ListView;
 import android.widget.RemoteViews;
 
 import com.appdiscovery.app.services.AppsWidgetService;
 import com.appdiscovery.app.services.DiscoverApp;
+import com.appdiscovery.app.services.LocationWatcher;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
 
 //TODO: refactor
 public class AppsWidgetProvider extends BroadcastReceiver {
-    private ListView mListView;
+    Context mContext;
     public static WebApp[] webapps = new WebApp[0];
-
-    private View.OnClickListener onListItemClick = (view -> {
-        int itemPosition = mListView.getSelectedItemPosition();
-        WebApp webapp = webapps[itemPosition];
-        try {
-            MainActivity.activeAppName = webapp.name;
-            webapp.launch();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    });
 
     private void setListView(WebApp[] webapps) {
         this.webapps = webapps;
+        updateWidgetView();
     }
 
-    private void discoverAppByLocation(Location location, Context context) {
+    private LocationWatcher mLocationWatcher = null;
+
+    private void discoverAppByLocation(Location location) {
         DiscoverApp.byLocation(location, webapps -> {
             for (WebApp webapp : webapps) {
                 new Thread(() -> {
@@ -64,7 +52,7 @@ public class AppsWidgetProvider extends BroadcastReceiver {
         });
     }
 
-    private void discoverAppByLan(Context context) {
+    private void discoverAppByLan() {
         DiscoverApp.byLan(webapps -> {
             for (WebApp webapp : webapps) {
                 new Thread(() -> {
@@ -87,7 +75,11 @@ public class AppsWidgetProvider extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("intent.action", intent.getAction());
+        if (this.mLocationWatcher == null) {
+            this.mLocationWatcher = new LocationWatcher(context, this::discoverAppByLocation);
+            mContext = context;
+            mLocationWatcher.start();
+        }
         if ("SHOW_APP".equals(intent.getAction())) {
             int position = intent.getIntExtra("position", -1);
             WebApp webapp = webapps[position];
@@ -99,24 +91,24 @@ public class AppsWidgetProvider extends BroadcastReceiver {
             }
         } else if ("UPDATE_WIDGET".equals(intent.getAction())) {
             WebApp.setContext(context);
-            this.discoverAppByLan(context);
-            RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.app_widget_layout);
-
-            Intent svcIntent = new Intent(context, AppsWidgetService.class);
-            svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
-            updateViews.setRemoteAdapter(R.id.widget_apps_list, svcIntent);
-            ComponentName thisWidget = new ComponentName(context, AppsWidgetProvider.class);
-            AppWidgetManager manager = AppWidgetManager.getInstance(context);
-            ComponentName thisAppWidget = new ComponentName(context.getPackageName(), AppsWidgetProvider.class.getName());
-            int[] appWidgetIds = manager.getAppWidgetIds(thisAppWidget);
-            manager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_apps_list);
-
-            Intent showAppIntent = new Intent(context, AppsWidgetProvider.class);
-            showAppIntent.setAction("SHOW_APP");
-            PendingIntent clickPI = PendingIntent.getBroadcast(context, 0, showAppIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            updateViews.setPendingIntentTemplate(R.id.widget_apps_list, clickPI);
-
-            manager.updateAppWidget(thisWidget, updateViews);
+            this.discoverAppByLan();
         }
+    }
+
+    private void updateWidgetView() {
+        RemoteViews updateViews = new RemoteViews(mContext.getPackageName(), R.layout.app_widget_layout);
+        Intent svcIntent = new Intent(mContext, AppsWidgetService.class);
+        svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
+        updateViews.setRemoteAdapter(R.id.widget_apps_list, svcIntent);
+        ComponentName thisWidget = new ComponentName(mContext, AppsWidgetProvider.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(mContext);
+        ComponentName thisAppWidget = new ComponentName(mContext.getPackageName(), AppsWidgetProvider.class.getName());
+        int[] appWidgetIds = manager.getAppWidgetIds(thisAppWidget);
+        manager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_apps_list);
+        Intent showAppIntent = new Intent(mContext, AppsWidgetProvider.class);
+        showAppIntent.setAction("SHOW_APP");
+        PendingIntent clickPI = PendingIntent.getBroadcast(mContext, 0, showAppIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        updateViews.setPendingIntentTemplate(R.id.widget_apps_list, clickPI);
+        manager.updateAppWidget(thisWidget, updateViews);
     }
 }
