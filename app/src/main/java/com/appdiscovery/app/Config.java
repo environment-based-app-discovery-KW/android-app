bd20b0401e18e148b5707b250c693ba5d79795b0
Ke Wang <453587854@qq.com>
2018-05-21 13:23:17 +0800

    update config

--- app/src/main/java/com/appdiscovery/app/Config.java
+++ app/src/main/java/com/appdiscovery/app/Config.java
@@ -7,8 +7,10 @@ public class Config {
         return ourInstance;
     }
 
-    public String repoServerAddr = "http://10.222.160.194:888";
-    public String centralServerAddr = "http://10.222.160.194:889";
+    public String repoServerAddr = "http://ad-app-repo-dynamic.kevinwang.cc:888";
+    public String centralServerAddr = "http://ad-central-server.kevinwang.cc:889";
+
+    public String lanServerAddr = "http://lan-app-repo-server.appd";
 
     private Config() {
     }
